731cb94d5f55e070d6fbd088dbeb0af627124da0
Cinthia Andrade <cinthia.andrade@insightlab.ufc.br>
2021-07-31 21:52:38 -0300

    feat[📌]: add Modal transactions

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 import iconLogo from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransitionModal: () => void;
+}
+export function Header({ onOpenNewTransitionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={iconLogo} alt="dt money" />
-        <button>Nova transação</button>
+        <button type="button" onClick={onOpenNewTransitionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
