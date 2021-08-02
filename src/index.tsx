bd3412a17543387fef565bdc98a8b6263c1782a8
Cinthia Andrade <cinthia.andrade@insightlab.ufc.br>
2021-08-02 00:08:45 -0300

    feat[🎉]:finished formatting the data

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,29 @@ createServer({
   models: {
     transaction: Model,
   },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freela de web",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1000,
+          createdAt: new Date("2021-05-12 11:00:00"),
+        },
+      ],
+    });
+  },
   routes() {
     this.namespace = "api";
     this.get("/transactions", () => {
